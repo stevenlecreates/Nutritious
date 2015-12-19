9ca6c9fe8f7d06fffa04e117feeadda1abd7dc16
Steven Le <stevenlecreates@gmail.com>
2015-12-19 10:42:30 +0000

    Added form and function so that submit button puts item in database

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,9 +1,20 @@
 class MealsController < ApplicationController
 	def index
-		
+		@meals = Meal.all
 	end
 
 	def new
-		@meal = Meal.new
+		@meals = Meal.new
+	end
+
+	def create
+		Meal.create(meal_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def meal_params
+		params.require(:meal).permit(:name, :ingredients, :instructions)
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219094840) do
+ActiveRecord::Schema.define(version: 20151219102334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "foods", force: true do |t|
+    t.string   "name"
+    t.string   "ingredients"
+    t.string   "instructions"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "meals", force: true do |t|
     t.string   "name"
     t.string   "ingredients"
     t.string   "instructions"
